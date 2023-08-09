a75eb057256f8b3860d0b2d7d27f673c53e795b0
Charlie Coppinger <charles.coppinger@gmail.com>
2023-08-09 18:19:58 +1200

    finish desktop view

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
+            backgroundImage: {
+                "twitch-pattern": "url('/twitch-pattern.png')",
+            },
             fontFamily: {
                 glamour: ["Glamour", "serif"],
                 satoshi: ["Satoshi", "sans-serif"],
