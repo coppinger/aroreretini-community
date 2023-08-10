87396223592f19ef9b3031557b4846703aff1631
Charlie Coppinger <charles.coppinger@gmail.com>
2023-08-10 21:49:58 +1200

    add contributors

---
+++ src/data/donations.js
@@ -0,0 +1,81 @@
+const donations = [
+    {
+        name: "whitep4nth3r",
+        amount: "100",
+        socials: {
+            twitch: "https://twitch.tv/whitep4nth3r",
+            twitter: "https://twitter.com/whitep4nth3r",
+        },
+        pfp: "https://pbs.twimg.com/profile_images/1676137555555647488/GnqLuxNX_400x400.jpg",
+    },
+    {
+        name: "AkiraLaine",
+        amount: "100",
+        socials: {
+            twitch: "https://twitch.tv/akiralaine",
+        },
+        pfp: "https://github.com/darthmeme.png",
+    },
+    {
+        name: "creepio",
+        socials: {
+            twitch: "https://twitch.tv/threecreepio",
+            twitter: "https://twitter.com/threecreepio",
+        },
+        amount: "50",
+        pfp: "https://pbs.twimg.com/profile_images/1130887322189336576/jtswkWQc_400x400.png",
+    },
+    {
+        name: "mono_nz",
+        socials: {
+            twitter: "https://twitter.com/mononzz",
+            twitch: "https://www.twitch.tv/mono_nz",
+        },
+        amount: "50",
+        pfp: "https://pbs.twimg.com/profile_images/1596421733988253696/gnw6fMks_400x400.jpg",
+    },
+    {
+        name: "Greg Gilbert",
+        amount: "50",
+        socials: {
+            twitter: "https://twitter.com/greggilbertme",
+        },
+        pfp: "https://pbs.twimg.com/profile_images/1688833453943308288/EJaiyrjj_400x400.jpg",
+    },
+    {
+        name: "ManniMoki",
+        socials: {
+            twitch: "https://twitch.tv/ManniMoki",
+            twitter: "https://twitter.com/ManniMoki",
+        },
+        amount: "50",
+        pfp: "https://pbs.twimg.com/profile_images/1411181356919443458/yxJ0RVry_400x400.jpg",
+    },
+    {
+        name: "Leon",
+        socials: {
+            twitter: "https://twitter.com/LeonTwizzle",
+            twitch: "https://twitch.tv/leond53",
+        },
+        amount: "30",
+        pfp: "https://pbs.twimg.com/profile_images/1502889467165933568/j_ekWrCu_400x400.jpg",
+    },
+    {
+        name: "Kroucher",
+        socials: {
+            twitch: "https://twitch.tv/kroucher",
+        },
+        amount: "30",
+        pfp: "https://static-cdn.jtvnw.net/jtv_user_pictures/ce98d4f0-bfe0-48d6-ba01-9bb3e0b40450-profile_image-70x70.png",
+    },
+    {
+        name: "Anonmous",
+        socials: {
+            none: "true",
+        },
+        amount: "10",
+        pfp: "",
+    },
+];
+
+export default donations;
